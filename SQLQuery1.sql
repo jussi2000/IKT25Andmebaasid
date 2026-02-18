8de937e2b69e431a5965f219d05e76c8d7982a60
Opilane TTHK <johannat2009@gmail.com>
2026-02-18 11:36:08 +0200

    Andmebaasid2

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,7 @@
+     
+	    -- Tund 11.02.26 --
+---------------------------------------
+
 -- teeme andmebaas'i e. db(data Base)
 create database IKT25TAR
 
@@ -39,7 +43,7 @@ values (1, 'Male'),
 (2, 'Female')
 
 -- Vaatame tabeli sisu
--- * tähendab, et näita kõike seal sees olevat infot
+-- '*' tähendab, et näita kõike seal sees olevat infot
 select * from Gender
 
 -- teeme uue tabeli nimega 'Person'
@@ -55,3 +59,124 @@ Name nvarchar(30),
 Email nvarchar(30),
 GenderId int
 )
+
+insert into Person (id, Name, Email, GenderId)
+values (1, 'Superman', 's@s.com', 2),
+(2, 'Wonerwoman', 'w@w.com', 1),
+(3, 'Batman', 'b@b.com', 2),
+(4, 'Aquaman', 'a@a.com', 2),
+(5, 'CatWoman', 'c@c.com', 1),
+(6, 'Antman', 'ant"ant.com', 2),
+(8, NULL, NULL, 2)
+
+-- näen tabelis olevat infot
+select * from Person
+
+-- võõrvõtme ühenduse loomine kahe tabeli vahel
+alter table person add constraint tblPerson_GenderId_FK
+foreign key(GenderId) references Gender(Id)
+
+-- kui sisestad uue rea andmeid ja ei ole sisestanud GenderId alla
+-- väärtust, siis automaatselt sisestab sellele reale väärtuse 3
+-- e. unknown
+alter table person
+add constraint DF_Person_GénderId
+default 3 for GenderId
+
+insert into Gender(id, Gender)
+values (3, 'Unknown')
+
+insert into Person(id, Name, Email, GenderId)
+values (7, 'Black Panter', 'b@b.com', NULL)
+
+insert into Person(id, Name, Email)
+values (9, 'Spiderman', 'spider@man.com')
+
+select * from Person
+
+-- piirangu kasutamine
+alter table person
+drop constraint DF_Persons_GeñderId
+
+-- kuidas lisada veergu tabelile Person
+--veeru nimi on Age nvarchar(10)
+alter table Person
+add Age nvarchar(10)
+
+alter table person
+add constraint CK_Person_Age check (Age > 0 and Age < 155)
+
+-- kuidas uuendada andmeid
+update Person
+set Age = 159
+where id = 7
+
+select * from Person
+
+-- soovin kustutada ühe rea
+-- kuidas seda teha?
+
+delete from Person where id = 8
+
+-- lisame uue veeru City nvarchar(50)
+alter table Person
+add City nvarchar(50)
+
+-- kõik, kes elavad Gothami linnas
+select * from Person where City = 'Gotham'
+-- kõik, kes ei ela Gothamis
+select * from Person where City! = 'Gotham'
+-- varjant nr 2. kõik, kes ei ela Gothamis
+select * from Person where City <> 'Gotham'
+
+-- Näitab teatud vanusega inimesi
+-- Valime 151, 35, 25
+
+-- Vrs. 1
+select * from Person where Age in (151, 35, 26)
+
+-- Vrs. 2
+select * from Person where Age = 151 or Age = 35 or Age = 26
+
+-- Soovin näha inimesi vahemikus 22 kuni 41
+
+-- Vrs. 1
+select * from Person where Age between 22 and 41
+
+-- Vrs. 2
+select * from Person where Age >= 22 and Age <= 41
+
+-- wildcard e. näitab kõik g-tähega linnad
+select * from Person where City like 'G%'
+-- otsib emailidm @-märgiga
+select * from Person where Email like '%@%'
+
+-- '%' näitab asukohta
+-- tahan näha, kellel on emailis ees ja peale @-märki üks täht
+select * from Person where Email like '_@_.%'
+
+-- kõik, kelle nimes ei ole esimene täht W, A, S
+select * from Person where Name like '[^WAS]%'
+
+-- kõik, kes elavad Gothamis ja New Yorkis
+-- vrs. 1
+select * from Person where (City = 'Gotham' or City = 'New York')
+-- vrs. 2
+select * from Person where City in ('Gotham', 'New York')
+
+-- kõik, kes elavad Gothamis ja New Yorkis ning peavad olema
+-- vanemad, kui 29
+-- vrs. 1
+select * from Person where (City = 'Gotham' or City = 'New York') 
+and Age >= 30
+-- vrs. 2
+select * from Person where (City = 'Gotham' or City = 'New York') 
+and (Age > 29)
+
+-- Kuvab tähestikulises järjekorras inimesi ja võtab aluseks
+-- Name veeru
+select * from Person
+select * from Person order by Name
+
+-- Võtab kolm esimest rida Person tabelist
+select top 3 * from Person
